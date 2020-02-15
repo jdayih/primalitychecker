74157e2be60ebd939df1ae88c30e9a242ba00870
Jasmine <jasminedhaliwal29@gmail.com>
2020-02-15 23:36:32 +0000

    Update primalitychecker.py

--- primalitychecker.py
+++ primalitychecker.py
@@ -1,7 +1,4 @@
-Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def primality_checker():
+def primality_checker():
 	number = int(input("Enter a number: "))
 	divisors = []
 	for i in range(1,number+1):
